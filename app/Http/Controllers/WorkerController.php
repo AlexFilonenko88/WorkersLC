7c9d8aa17708de3dcbec740213c244053da16eb2
AlexFilonenko88 <af88_88@mail.ru>
2024-08-28 14:45:21 +0300

    lesson 6

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,17 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Worker;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
     public function index()
     {
-        return "Test WorkerController";
+        $workers = Worker::all();
+        foreach ($workers as $worker) {
+//            dump($worker->name);
+        }
     }
 
     public function show()
     {
-        return "Test WorkerController, show action";
+        $worker = Worker::find(2);
+        dd($worker);
+    }
+
+    public function create()
+    {
+        $worker = [
+            'name' => 'Mark',
+            'surname' => 'Markov',
+            'email' => 'markov@mail.ru',
+            'age' => '20',
+            'description' => 'Test',
+            'is_married' => false,
+        ];
+
+        Worker::create($worker);
+
+        return "Ivan was created";
+    }
+
+    public function update()
+    {
+        $worker = Worker::find(2);
+        $worker->update([
+            'name' => 'Karl',
+            'surname' => 'Petrov',
+        ]);
+
+        return 'Was updated';
+    }
+
+    public function delete()
+    {
+        $worker = Worker::find(2);
+        $worker->delete();
+
+        return 'Was deleted';
     }
 }

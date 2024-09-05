22f68dda3eada39534d2aaca21caaa7d8e0bed0d
AlexFilonenko88 <af88_88@mail.ru>
2024-09-05 17:56:41 +0300

    lesson 13

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,16 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Worker\IndexRequest;
 use App\Http\Requests\Worker\StoreRequest;
 use App\Http\Requests\Worker\UpdateRequest;
 use App\Models\Worker;
+
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    public function index()
+    public function index(IndexRequest $request)
     {
-        $workers = Worker::paginate(4);
+        $data = $request->validated();
+
+        $workerQuery = Worker::query();
+
+        if (isset($data['name'])) {
+            $workerQuery->where('name', 'like', "%{$data['name']}%");
+        }
+
+        if (isset($data['surname'])) {
+            $workerQuery->where('surname', 'like', "%{$data['surname']}%");
+        }
+
+        if (isset($data['email'])) {
+            $workerQuery->where('email', 'like', "%{$data['email']}%");
+        }
+
+        if (isset($data['from'])) {
+            $workerQuery->where('age', '>', $data['from']);
+        }
+
+        if (isset($data['to'])) {
+            $workerQuery->where('age', '<', $data['to']);
+        }
+
+        if (isset($data['description'])) {
+            $workerQuery->where('description', 'like', "%{$data['description']}%");
+        }
+
+        if (isset($data['is_married'])) {
+            $workerQuery->where('age', true);
+        }
+
+        $workers = $workerQuery->paginate(4);
+
 
         return view('worker.index', compact('workers'));
     }

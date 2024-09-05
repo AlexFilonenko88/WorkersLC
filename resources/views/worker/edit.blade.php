777e88c4bf02e6dc807c0ccac0e534ee93bf634e
AlexFilonenko88 <af88_88@mail.ru>
2024-09-05 12:51:03 +0300

    lesson 11

--- resources/views/worker/edit.blade.php
+++ resources/views/worker/edit.blade.php
@@ -16,27 +16,57 @@ Create page
             @csrf
             @method('Patch')
             <div style="margin-bottom: 15px">
-                <input type="text" name="name" placeholder="name" value="{{$worker->name}}">
+                <input type="text" name="name" placeholder="name" value="{{ old('name') ?? $worker->name}}">
+                @error('name')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
-                <input type="text" name="surname" placeholder="surname" value="{{$worker->surname}}">
+                <input type="text" name="surname" placeholder="surname" value="{{old('surname') ?? $worker->surname}}">
+                @error('surname')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
-                <input type="email" name="email" placeholder="email" value="{{$worker->email}}">
+                <input type="email" name="email" placeholder="email" value="{{old('email') ?? $worker->email}}">
+                @error('email')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
-                <input type="number" name="age" placeholder="age" value="{{$worker->age}}">
+                <input type="number" name="age" placeholder="age" value="{{old('age') ?? $worker->age}}">
+                @error('age')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
                 <textarea name="description" placeholder="description">
-                    {{$worker->description}}
+                    {{old('description') ?? $worker->description}}
                 </textarea>
+                @error('description')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
                 <input id="isMarried" type="checkbox" name="is_married"
-                    {{ $worker->is_married ? 'checked' : '' }}
+                        {{ $worker->is_married ? 'checked' : '' }}
                 >
-                <label for="isMarried">Is married</label>
+                <label for="is_married">Is married</label>
+                @error('description')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div style="margin-bottom: 15px">
                 <input type="submit" value="Сохранить">

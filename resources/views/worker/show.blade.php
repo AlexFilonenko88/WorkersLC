f71a0947dc42e83dbaf007a2375d8ca27869b0f2
AlexFilonenko88 <af88_88@mail.ru>
2024-09-06 15:16:13 +0300

    lesson 15

--- resources/views/worker/show.blade.php
+++ resources/views/worker/show.blade.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-Index page
+@extends('layout.main')
+@section('content')
 <div>
     <hr>
     <div>
@@ -24,5 +15,4 @@ Index page
     </div>
     <hr>
 </div>
-</body>
-</html>
+@endsection
